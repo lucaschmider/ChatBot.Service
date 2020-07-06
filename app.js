2d141983eeda3619150034b82282f03f128f7547
Luca Schmider <luca.schmider@gmail.com>
2020-07-06 20:03:50 +0200

    Created configservice

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const chalk = require("chalk");
 const express = require("express");
 const mongoose = require("mongoose");
 const controllers = require("./Controllers");
+const loadConfiguration = require("./ConfigService");
 const app = express();
 
 async function startup() {
+  console.log(chalk.yellow(`Starting application for environment ${process.chatBot.environment}`));
   console.log(chalk.yellow("Connecting to MongoDB"));
 
   try {
-    await mongoose.connect("mongodb://localhost/chatbot", {
+    await mongoose.connect(process.chatBot.databaseUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
@@ -20,7 +22,16 @@ async function startup() {
 
   app.use("/", controllers);
 
-  app.listen(3000, () => console.log("Listening"));
+  app.listen(process.chatBot.applicationPort, () =>
+    console.log(`Listening for requests at *:${process.chatBot.applicationPort}`)
+  );
 }
 
-startup();
+loadConfiguration(process.argv[2])
+  .then((configuration) => {
+    process.chatBot = configuration;
+    startup();
+  })
+  .catch((error) => {
+    console.log(chalk.red(error));
+  });
