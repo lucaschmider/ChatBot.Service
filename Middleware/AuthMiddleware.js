1b1a70971305d9a688740bb2850209ab3f610510
Luca Schmider <luca.schmider@gmail.com>
2020-07-10 08:29:39 +0200

    Implemented AuthMiddleware

---
+++ Middleware/AuthMiddleware.js
@@ -0,0 +1,27 @@
+const admin = require("../Services/Firebase");
+
+const getAuthToken = (req, res, next) => {
+  if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
+    req.authToken = req.headers.authorization.split(" ")[1];
+  } else {
+    req.authToken = null;
+  }
+  next();
+};
+
+const verifyIdToken = (req, res, next) => {
+  getAuthToken(req, res, async () => {
+    console.log(req.authToken);
+    try {
+      const { authToken } = req;
+      const userInfo = await admin.auth().verifyIdToken(authToken);
+      req.userId = userInfo.uid;
+      return next();
+    } catch (e) {
+      console.log(e);
+      return res.status(401).send({ error: "You are not authorized to make this request" });
+    }
+  });
+};
+
+module.exports = verifyIdToken;
---
+++ Services/Firebase.js
@@ -0,0 +1,9 @@
+const admin = require("firebase-admin");
+
+const firebaseConfig = {};
+
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseConfig)
+});
+
+module.exports = admin;
