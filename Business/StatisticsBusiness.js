67ef783b736010690991831dbe4e6e036e30730d
Luca Schmider <luca.schmider@gmail.com>
2020-07-17 13:41:36 +0200

    Added dummy feedback option

--- Business/StatisticsBusiness.js
+++ Business/StatisticsBusiness.js
@@ -1,10 +1,24 @@
 const { StatisticsRepository } = require("../Repository/StatisticsRepository.js");
+const { UserRepository } = require("../Repository/UserRepository");
 
 class StatisticsBusiness {
-  GetUserSatisfactionAsync = async () => {
+  static async GetUserSatisfactionAsync() {
     const data = await StatisticsRepository.GetStatisticsAsync();
     return data;
-  };
+  }
+
+  static async ProccessFeedbackAsync(userId, rating) {
+    const userData = await UserRepository.GetUserDataForUserAsync(userId);
+    const department = userData.department;
+
+    const ratingData = {
+      rating,
+      department
+    };
+
+    console.log(ratingData);
+    throw new Error("Not implemented!");
+  }
 }
 
 module.exports = { StatisticsBusiness };
--- Controllers/StatisticsController.js
+++ Controllers/StatisticsController.js
@@ -1,5 +1,6 @@
+const chalk = require("chalk");
 const { Router } = require("express");
-const statisticsBusiness = require("../Business/StatisticsBusiness");
+const { StatisticsBusiness } = require("../Business/StatisticsBusiness");
 
 class StatisticsController {
   /**
@@ -9,6 +10,7 @@ class StatisticsController {
   static getRouter() {
     const router = Router();
     router.get("/", StatisticsController.GetUserSatisfactionAsync);
+    router.post("/feedback", StatisticsController.CreateFeedbackAsync);
     return router;
   }
 
@@ -18,8 +20,23 @@ class StatisticsController {
    * @param {Response} res
    */
   static async GetUserSatisfactionAsync(req, res) {
-    const data = await statisticsBusiness.GetUserSatisfactionAsync();
+    const data = await StatisticsBusiness.GetUserSatisfactionAsync();
     res.send(data);
+  }
+
+  /**
+   * Saves anonymous user satisfaction data
+   * @param {Request} req
+   * @param {Response} res
+   */
+  static async CreateFeedbackAsync(req, res) {
+    try {
+      await StatisticsBusiness.ProccessFeedbackAsync(req.userData.uid, req.body.rating);
+      res.status(204).send();
+    } catch (error) {
+      console.log(chalk.red("An unhandled error occured:\n"), error);
+      res.status(500).send();
+    }
   }
 }
 
