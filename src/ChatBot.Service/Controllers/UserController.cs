3a0198f2b8d73ad02c6fd31aa57fe5bdd218cc98
Luca Schmider <luca.schmider@gmail.com>
2020-08-08 22:53:49 +0200

    Added user controller

---
+++ src/ChatBot.Service/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ChatBot.Service.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBot.Service.Controllers
+{
+    /// <summary>
+    ///     Provided methods to manage users
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        ///     Returns details about the user that is currently signed in
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Authorize]
+        [ProducesResponseType(typeof(UserDetails), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetUserDataAsync()
+        {
+            var userId = User.Claims.FirstOrDefault(claim => claim.Type == "user_id")?.Value;
+            return await Task.FromResult(Ok(userId));
+        }
+    }
+}
