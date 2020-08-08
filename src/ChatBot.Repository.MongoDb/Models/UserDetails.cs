0d43d9e4e8609f2229f575d13f2f3d44caa39d61
Luca Schmider <luca.schmider@gmail.com>
2020-08-09 00:19:27 +0200

    added error handling

--- src/ChatBot.Repository.MongoDb/Models/UserDetails.cs
+++ src/ChatBot.Repository.MongoDb/Models/UserDetails.cs
@@ -1,17 +1,12 @@
 ﻿using ChatBot.Repository.Contracts.Models;
-using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace ChatBot.Repository.MongoDb.Models
 {
     /// <inheritdoc cref="IUserDetails" />
     [BsonIgnoreExtraElements]
-    public class UserDetails :IUserDetails
+    public class UserDetails : IUserDetails
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string DataSetId { get; set; }
-
         [BsonElement("uid")] public string Uid { get; set; }
 
         [BsonElement("isAdmin")] public bool IsAdmin { get; set; }
@@ -21,6 +16,5 @@ namespace ChatBot.Repository.MongoDb.Models
         [BsonElement("department")] public string Department { get; set; }
 
         [BsonElement("email")] public string Email { get; set; }
-        public string CollectionName => "users";
     }
 }
--- src/ChatBot.Service/Controllers/UserController.cs
+++ src/ChatBot.Service/Controllers/UserController.cs
@@ -1,9 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using ChatBot.Repository.Contracts;
 using ChatBot.Service.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shouldly;
 
 namespace ChatBot.Service.Controllers
 {
@@ -14,11 +17,15 @@ namespace ChatBot.Service.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly ILogger<UserController> _logger;
         private readonly IUserRepository _userRepository;
 
-        public UserController(IUserRepository userRepository)
+        public UserController(IUserRepository userRepository, ILogger<UserController> logger)
         {
+            userRepository.ShouldNotBeNull();
+            logger.ShouldNotBeNull();
             _userRepository = userRepository;
+            _logger = logger;
         }
 
         /// <summary>
@@ -32,9 +39,30 @@ namespace ChatBot.Service.Controllers
         [ProducesResponseType(500)]
         public async Task<IActionResult> GetUserDataAsync()
         {
-            var userId = User.Claims.FirstOrDefault(claim => claim.Type == "user_id")?.Value;
-            var userData = await _userRepository.GetUserDetailsAsync(userId);
-            return Ok(userData);
+            try
+            {
+                _logger.LogInformation("Getting user details");
+                var userId = User.Claims.FirstOrDefault(claim => claim.Type == "user_id")?.Value;
+
+                if (userId == null)
+                {
+                    _logger.LogWarning("Could not determine userid from token");
+                    return BadRequest();
+                }
+
+
+                var userData = await _userRepository.GetUserDetailsAsync(userId);
+
+                if (userData != null) return Ok(userData);
+                
+                _logger.LogWarning($"Could not find data for uid {userId}.");
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Unexpected error occured while getting user details", ex);
+                return StatusCode(500);
+            }
         }
     }
 }
